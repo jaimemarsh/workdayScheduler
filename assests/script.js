fbd13feecbe20e3611a9882122255aa8366371e8
Jaime M <jaimemarsh19@gmail.com>
2023-02-27 18:30:40 -0500

    Cleaning up code and fixed bug for present time color

--- assests/script.js
+++ assests/script.js
@@ -1,59 +1,28 @@
-
-
-// TODO: Add a listener for click events on the save button. This code should
-// use the id in the containing time-block as a key to save the user input in
-// local storage. HINT: What does `this` reference in the click listener
-// function? How can DOM traversal be used to get the "hour-x" id of the
-// time-block containing the button that was clicked? How might the id be
-// useful when saving the description in local storage?
-//
-
-
-// TODO: Add code to get any user input that was saved in localStorage and set
-// the values of the corresponding textarea elements. HINT: How can the id
-// attribute of each time-block be used to do this?
-
-
-
-
-
-// TODO: Add code to display the current date in the header of the page.
-// Display Current Date
 var currentTime = dayjs().format('dddd MMMM, MM')
 $("#currentDay").text(currentTime);
 console.log(currentTime);
 
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
 $(document).ready(function () {
 
-    // click event listener 
-    $('#Btn').on("click", function () {
+    $("Button").click(function () {
         console.log("Button was pressed")
-        var text = $(this).sibling(".description").val();
         var time = $(this).parent().attr("id");
-
+        var text = $(this).siblings(".description").val();
+        // Save text in local storage
         localStorage.setItem(time, text);
     })
-
-
-    // TODO: Add code to apply the past, present, or future class to each time
-    // block by comparing the id to the current hour. HINTS: How can the id
-    // attribute of each time-block be used to conditionally add or remove the
-    // past, present, and future classes? How can Day.js be used to get the
-    // current hour in 24-hour time?
 
     // Hours in local storage 
     $("#hour9 .description").val(localStorage.getItem("hour9"));
     $("#hour10 .description").val(localStorage.getItem("hour10"));
     $("#hour11 .description").val(localStorage.getItem("hour11"));
     $("#hour12 .description").val(localStorage.getItem("hour12"));
-    $("#hour1 .description").val(localStorage.getItem("hour1"));
-    $("#hour2 .description").val(localStorage.getItem("hour2"));
-    $("#hour3 .description").val(localStorage.getItem("hour3"));
-    $("#hour4 .description").val(localStorage.getItem("hour4"));
-    $("#hour5 .description").val(localStorage.getItem("hour5"));
+    $("#hour13 .description").val(localStorage.getItem("hour13"));
+    $("#hour14 .description").val(localStorage.getItem("hour14"));
+    $("#hour15 .description").val(localStorage.getItem("hour15"));
+    $("#hour16 .description").val(localStorage.getItem("hour16"));
+    $("#hour17 .description").val(localStorage.getItem("hour17"));
+    $("#hour18 .description").val(localStorage.getItem("hour18"));
 
 
     function theSchedule() {
@@ -72,7 +41,7 @@ $(document).ready(function () {
                 $(this).addClass("past");
             }
 
-            else if (notTime < exactTime) {
+            else if (notTime = exactTime) {
                 $(this).removeClass("future");
                 $(this).removeClass("past")
                 $(this).addClass("present");
